cc8b0a4a46e2486e89af4deb9823af880b4e7e55
Alex Yakubov <yakubov.web@yandex.ru>
2021-05-06 12:50:35 +0300

    Home Work #1

---
+++ admin = name/skript.js
@@ -0,0 +1,3 @@
+var name = "Василий";
+var admin = name;
+alert(admin)
---
+++ обмен переменных/script.js
@@ -0,0 +1,16 @@
+
+var a = 10
+var b = 20
+alert("a = " + a + " b = " + b)
+
+a = a + b
+// a = 30
+b = a - b
+// b = 10
+a = a - b
+//a = 20
+
+alert("a = " + a + " b = " + b)
+
+var a = 1000 + "108"
+alert(a)
